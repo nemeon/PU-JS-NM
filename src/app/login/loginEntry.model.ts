26834ccd7874b4a70b3a2518a3bbe50c5f92bed7
unknown <nasko.maznev@gmail.com>
2018-05-30 22:28:28 +0300

    First view of Angeular site for GitPages

--- src/app/login/loginEntry.model.ts
+++
@@ -1,9 +0,0 @@
-export class LoginEntry {
-    email: string;
-    password: string;
-
-    constructor(email?: string, password?: string) {
-        this.email = email;
-        this.password = password;
-    }
-}
